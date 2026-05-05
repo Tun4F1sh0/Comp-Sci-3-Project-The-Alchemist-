154a1cb66985142823873861d7e0e001e44c7b09
Tun4F1sh0 <cheesytuan1021@gmail.com>
2026-05-05 18:46:49 -0500

    Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
+            
+            Scene scene = new Scene(root);
+            
+            primaryStage.setTitle("The Alchemist");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+        } catch (IOException e) {
+            System.err.println("Error: Could not find or load the FXML file.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
